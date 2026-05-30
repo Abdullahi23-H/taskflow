1411e024e1eb5c8cac09efee0166ed8d5580978d
abdullahi mahad <abdullahimahad483@gmail.com>
2026-05-30 11:49:02 +0100

    feat: add get, update, delete workspace by id

--- apps/api/src/modules/workspaces/workspaces.routes.ts
+++ apps/api/src/modules/workspaces/workspaces.routes.ts
@@ -55,5 +55,93 @@ workspacesRouter.get("/", async(req,res)=>{
         
 })
 
+const updateWorkSpaceSchema = z.object({
+    name: z.string().min(1).max(100),
+})
+workspacesRouter.get("/:id", async(req,res)=>{
+    try{
+        const  workspace = await prisma.workspace.findFirst({
+            where:{
+                id: req.params.id,
+                ownerId: req.userId!
+
+            },
+            select:{
+                id:true,
+                name:true,
+                ownerId:true,
+                createdAt:true,
+                updatedAt:true,
+            }
+        })
+        if(!workspace){
+            return res.status(404).json({ error: "Workspace not found" })
+        }
+        return res.json({workspace});
 
 
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({error: "Internal server error"});
+    }
+});
+
+workspacesRouter.patch("/:id",async(req,res)=>{
+    try{
+        const  body = updateWorkSpaceSchema.parse(req.body);
+        const existing = await prisma.workspace.findFirst({
+            where:{
+                id: req.params.id,
+                ownerId: req.userId!
+
+            }
+        });
+        if(!existing){
+            return res.status(404).json({error: "Workspace not found"});
+        }
+        const workspace =  await prisma.workspace.update({
+            where:{id:existing.id},
+            data:{name:body.name},
+            select:{
+                id:true,
+                name:true,
+                ownerId:true,
+                createdAt:true,
+                updatedAt:true,
+            }
+
+
+        })
+        return res.json({ workspace });
+
+    }catch(err){
+        if(err instanceof z.ZodError){
+            return res.status(400).json({error:"Invalid input", details: err.flatten()})
+    }
+    console.error(err);
+    return res.status(500).json({error: "Internal server error"});
+    }
+})
+
+
+workspacesRouter.delete("/:id", async(req,res)=>{
+    try{
+        const existing = await prisma.workspace.findFirst({
+            where:{
+                id: req.params.id,
+                ownerId: req.userId!
+            }
+        });
+        if(!existing){
+            return res.status(404).json({ error: "Workspace not found" }); 
+        }
+        await prisma.workspace.delete({
+            where:{id:existing.id},
+
+        });
+        return res.status(204).send();
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({error: "Internal server error"});
+    }
+});
